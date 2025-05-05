6718ba7c8c839544155e8d137331a18750f351b1
SaikiranFrnd <saikiran@frnd.app>
2025-05-06 00:50:33 +0530

    Initial Commit to branch

---
+++ SaiKiran-Portfolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SaiKiran-Portfolio
+//
+//  Created by Sai Kiran on 05/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
